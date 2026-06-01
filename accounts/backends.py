79c3b3117172b1e13042d5d9ff83ffcb0d852127
FazaRihan <fazarihan8@gmail.com>
2026-06-02 02:58:26 +0800

    Fix EmailBackend to support admin login

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.backends import ModelBackend
 from .models import Account
 
+from django.contrib.auth.backends import ModelBackend
+from .models import Account
+
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, email=None, password=None, **kwargs):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        # Support both 'email' kwarg and 'username' (untuk admin)
+        email = kwargs.get('email') or username
+        
+        if email is None or password is None:
+            return None
+        
         try:
             user = Account.objects.get(email=email)
         except Account.DoesNotExist:
@@ -11,6 +20,12 @@ class EmailBackend(ModelBackend):
         if user.check_password(password) and self.user_can_authenticate(user):
             return user
         return None
+    
+    def get_user(self, user_id):
+        try:
+            return Account.objects.get(pk=user_id)
+        except Account.DoesNotExist:
+            return None
     
     def user_can_authenticate(self, user):
         # Override: cek is_active
