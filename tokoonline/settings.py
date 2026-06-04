183f6d9fb4137f657759635c04e9a2f019738a87
FazaRihan <fazarihan8@gmail.com>
2026-06-04 23:14:31 +0800

    Fix Cloudinary config

--- tokoonline/settings.py
+++ tokoonline/settings.py
@@ -144,11 +144,22 @@ WHITENOISE_AUTOREFRESH = True
 WHITENOISE_ROOT = BASE_DIR / 'static'
 
 import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+# Cloudinary config
+cloudinary.config(
+    cloud_name = config('CLOUDINARY_CLOUD_NAME'),
+    api_key = config('CLOUDINARY_API_KEY'),
+    api_secret = config('CLOUDINARY_API_SECRET'),
+)
+
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': config('CLOUDINARY_API_KEY'),
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Media files Configuration
