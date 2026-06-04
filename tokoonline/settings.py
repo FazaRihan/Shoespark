8ecbc93bc83f715fb42b8d4c0af315d6e9f5158c
FazaRihan <fazarihan8@gmail.com>
2026-06-05 01:04:19 +0800

    fix: tambah CSRF_TRUSTED_ORIGINS

--- tokoonline/settings.py
+++ tokoonline/settings.py
@@ -29,7 +29,9 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['shoespark.up.railway.app', 'localhost', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://shoespark.up.railway.app']
+CSRF_TRUSTED_ORIGINS = [
+    'https://shoespark.up.railway.app',
+]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
