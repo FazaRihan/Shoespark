9fc5666ba7a1558706a30c6602626bd37b611a58
FazaRihan <fazarihan8@gmail.com>
2026-06-04 23:34:20 +0800

    Switch to PostgreSQL database

--- tokoonline/settings.py
+++ tokoonline/settings.py
@@ -91,11 +91,12 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default='sqlite:///db.sqlite3')
+    )
 }
 
 
